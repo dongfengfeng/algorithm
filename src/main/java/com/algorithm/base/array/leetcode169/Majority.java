1350186b4abcaa67f41ea9c733dbcbc130140c0b
dongfengfeng <dongff1992@163.com>
2020-03-26 23:44:08 +0800

    修改错误

--- src/main/java/com/algorithm/base/array/leetcode169/Majority.java
+++ src/main/java/com/algorithm/base/array/leetcode169/Majority.java
@@ -8,10 +8,11 @@ public class Majority {
     public int find(int[] arrays) {
         int tmp = arrays[0];
         int vote = 1;
-        for (int i=0; i<arrays.length; i++) {
+        for (int i=1; i<arrays.length; i++) {
             if (vote == 0) {
                 tmp = arrays[i];
-            } else if (arrays[i] == vote) {
+                vote++;
+            } else if (arrays[i] == tmp) {
                 vote++;
             } else {
                 vote--;
